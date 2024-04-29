d463da8965dd9fa634bf91d64817d3d297cc5bdc
chani <qkrcks0426@naver.com>
2024-04-29 10:24:09 +0900

    imealove

---
+++ src/main/java/com/baewhaUniversity/imealove/ImealoveApplication.java
@@ -0,0 +1,12 @@
+package com.baewhaUniversity.imealove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImealoveApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ImealoveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baewhaUniversity/imealove/ImealoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baewhaUniversity.imealove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImealoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
